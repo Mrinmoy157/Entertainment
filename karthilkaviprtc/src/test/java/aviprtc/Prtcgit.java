dddae3b3ddb301fe6ee766d8ff445b65a50316c2
mrinm <mrinm@MRINMOY>
2022-03-26 16:19:45 +0530

    enjoylife

---
+++ karthilkaviprtc/src/test/java/aviprtc/Prtcgit.java
@@ -0,0 +1,17 @@
+package aviprtc;
+
+import org.testng.annotations.Test;
+
+public class Prtcgit {
+	@Test
+	public void song() {
+		System.out.println("why this colaberi di");
+	}
+	public void mainhoona() {
+		System.out.println("kiska hai ye tumko intezar mai hoona");
+	}
+	public void ddlj() {
+		System.out.println("Dilwale dulhaniya le jayenge");
+	}
+
+}
